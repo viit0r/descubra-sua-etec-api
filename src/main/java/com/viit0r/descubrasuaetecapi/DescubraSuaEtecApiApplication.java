2377e255658faad8c93c80c60210a8b1d0091822
viit0r <vituuoliveira1@gmail.com>
2023-04-16 20:53:20 -0300

    Criação e configurações iniciais do projeto

---
+++ src/main/java/com/viit0r/descubrasuaetecapi/DescubraSuaEtecApiApplication.java
@@ -0,0 +1,13 @@
+package com.viit0r.descubrasuaetecapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescubraSuaEtecApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescubraSuaEtecApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viit0r/descubrasuaetecapi/DescubraSuaEtecApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viit0r.descubrasuaetecapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescubraSuaEtecApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
