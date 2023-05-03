e347340aa29c472829629af9e6ae619e24987a1c
viit0r <vituuoliveira1@gmail.com>
2023-05-03 17:24:43 -0300

    Ajuste final na criação do script base das tabelas do banco de dados

--- src/main/resources/db/migration/V1__Criando_tabelas.sql
+++ src/main/resources/db/migration/V1__Criando_tabelas.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS `curso` (
     `duracao_semestre` int,
     `eixo_tecnologico` varchar(255),
     `link` varchar(255) NOT NULL,
+    `pre_requisitos` varchar(700),
     PRIMARY KEY (`id_curso`)
 );
 
@@ -14,6 +15,7 @@ CREATE TABLE IF NOT EXISTS `unidade_endereco` (
     `tipo` int NOT NULL,
     `logradouro` varchar(255) NOT NULL,
     `numero` varchar(10) NOT NULL,
+    `complemento` varchar(50),
     `bairro` varchar(255) NOT NULL,
     `cep` varchar(9) NOT NULL,
     `estado` varchar(255) NOT NULL,
@@ -25,6 +27,7 @@ CREATE TABLE IF NOT EXISTS `unidade` (
   `id_unidade` bigint NOT NULL UNIQUE AUTO_INCREMENT,
   `id_endereco` bigint NOT NULL,
   `nome` varchar(255) NOT NULL,
+  `descricao` varchar(800),
   `site` varchar(255) NOT NULL,
   `telefone` char(11),
   PRIMARY KEY (`id_unidade`),
@@ -35,8 +38,11 @@ CREATE TABLE IF NOT EXISTS `relacao_unidade_curso` (
     `id_relacao` bigint NOT NULL UNIQUE AUTO_INCREMENT,
     `id_unidade` bigint NOT NULL,
     `id_curso` bigint NOT NULL,
-    `etim` tinyint,
+    `presencial` tinyint,
+    `semipresencial` tinyint,
     `online` tinyint,
+    `etim` tinyint,
+    `especializacao` tinyint,
     PRIMARY KEY (`id_relacao`),
     FOREIGN KEY (`id_unidade`) REFERENCES unidade(`id_unidade`),
     FOREIGN KEY (`id_curso`) REFERENCES curso(`id_curso`)
