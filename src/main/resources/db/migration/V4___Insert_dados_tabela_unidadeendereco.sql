47afeaa8719b8d87cd53fe20b5619f2cce4e89f6
viit0r <vituuoliveira1@gmail.com>
2023-05-03 23:51:52 -0300

    Adicionado script de insert de dados na tabela de unidade_endereco

---
+++ src/main/resources/db/migration/V4___Insert_dados_tabela_unidadeendereco.sql
@@ -0,0 +1,441 @@
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Dr. José Augusto de Souza e Silva",
+"s/n",
+"Jardim Parque Morumbi",
+"05712-040",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Nova Granada",
+"35",
+"Casa Verde",
+"02522-050",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Monsenhor Andrade",
+"798",
+"Brás",
+"03009-000",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Frederico Grotte",
+"322",
+"Jardim São Luis",
+"05818-270",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Prof. Lineu Prestes",
+"913",
+"Butantã",
+"05508-000",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, complemento, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Cruzeiro do Sul",
+"2630",
+"Prédio II",
+"Santana",
+"02030-100",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Igarapé Água Azul",
+"70",
+"Cidade Tiradentes",
+"08485-310",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Paulo Lorenzani",
+"s/n",
+"Parque Novo Mundo",
+"02180-021",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Feliciano de Mendonça",
+"290",
+"Guaianazes",
+"08460-365",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+3,
+"das Lágrimas",
+"2461",
+"São João Clímaco",
+"04232-000",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Virgínia Ferni",
+"400",
+"Itaquera",
+"08253-000",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Miguel Ignácio Curi",
+"s/n",
+"Vila Carmosina",
+"08295-005",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Benjamin de Tudela",
+"155",
+"Fazenda da Juta",
+"03977-408",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Condessa Elizabeth de Robiano",
+"5200",
+"Penha",
+"03704-000",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Bactória",
+"38",
+"Vila Formosa",
+"03472-100",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"dos Guaianazes",
+"1385",
+"Campos Elíseos",
+"01204-001",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Clóvis Bueno de Azevedo",
+"70",
+"Ipiranga",
+"04266-040",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Presidente Vargas",
+"s/n",
+"Perus",
+"05207-000",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Ferreira de Araújo",
+"527",
+"Pinheiros",
+"05428-001",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Feliciano Correa",
+"s/n",
+"Jardim Satélite",
+"04815-240",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Jairo de Almeida Machado",
+"401",
+"Jaraguá",
+"02998-060",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+3,
+"da Baronesa",
+"1695",
+"Jardim Ângela",
+"04941-175",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Chucri Zaidan",
+"40",
+"Brooklin",
+"04583-110",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Américo Vespucci",
+"1241",
+"Vila Prudente",
+"03135-010",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Dr. Luis Lustosa da Silva",
+"303",
+"Mandaqui",
+"02406-040",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Apucarana",
+"815",
+"Tatuapé",
+"03311-000",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Ulisses Cruz",
+"85",
+"Belenzinho",
+"03077-000",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, complemento, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Cruzeiro do Sul",
+"2630",
+"Prédio I",
+"Santana",
+"02030-100",
+"São Paulo",
+"SP");
+
+ INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Elísio Teixeira Leite",
+"3611",
+"Jardim Paulistano",
+"02810-000",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Abilene",
+"16",
+"Parque Santo Antônio",
+"03385-160",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Dr. Orêncio Vidigal",
+"212",
+"Penha",
+"03640-010",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Guaipá",
+"678",
+"Vila Leopoldina",
+"05089-000",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Marcial",
+"25",
+"Mooca",
+"03169-040",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Alcântara",
+"113",
+"Vila Guilherme",
+"02110-010",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Ambrósia do México",
+"180",
+"Pirituba",
+"02945-040",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Cachoeira Poraquê",
+"326",
+"Raposo Tavares",
+"05574-450",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"General Couto de Magalhães",
+"145",
+"Santa Ifigênia",
+"01212-030",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Soledade de Minas",
+"87",
+"São Mateus",
+"03930-070",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Tiradentes",
+"615",
+"Luz",
+"01101-010",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+4,
+"Nothmann",
+"598",
+"Campos Elíseos",
+"01216-000",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Mário Lopes Leão",
+"1050A",
+"Santo Amaro",
+"04754-010",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Waldemar Tietz",
+"1477",
+"Artur Alvim",
+"03589-001",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+1,
+"Nazir Miguel",
+"779",
+"Jardim Paulo VI",
+"05570-030",
+"São Paulo",
+"SP");
+
+INSERT INTO unidade_endereco (tipo, logradouro, numero, bairro, cep, estado, uf) 
+VALUES (
+2,
+"Águia de Haia",
+"2633",
+"Cidade A.E. Carvalho",
+"03694-000",
+"São Paulo",
+"SP");
