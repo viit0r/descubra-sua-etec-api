032095dbc08cebc1041a90d446873392485b432a
viit0r <vituuoliveira1@gmail.com>
2023-05-04 20:32:23 -0300

    Adicionado script de insert de dados na tabela de unidade

---
+++ src/main/resources/db/migration/V5__Insert_dados_tabela_unidade.sql
@@ -0,0 +1,364 @@
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+1,
+"Etec Abdias do Nascimento",
+"1135077491 / 1135014994",
+"https://www.cps.sp.gov.br/etecs/etec-abdias-do-nascimento-paraisopolis/",
+"e224dir@cps.sp.gov.br"
+);
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+2,
+"Etec Albert Einstein",
+"1139660503 / 1138587529",
+"https://www.cps.sp.gov.br/etecs/etec-albert-einstein-casa-verde/",
+"e023dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, descricao, telefone, site, email)
+VALUES (
+3,
+"Etec Carlos de Campos",
+"Criada no dia 28 de setembro de 1911, passou a funcionar no ano seguinte. Surgiu exclusivamente para mulheres, num bairro cuja atividade fabril era intensa e onde o anarquismo ganhava espaço. A Etec se destacou também pela criação, em 1931, do Dispensário de Puericultura, cujo objetivo era atender às crianças da comunidade.",
+"1132270286 / 1133117098",
+"https://www.cps.sp.gov.br/etecs/etec-carlos-de-campos-bras/",
+"e045dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, descricao, telefone, site, email)
+VALUES (
+4,
+"Etec Carolina Carinhato Sampaio (antiga Etec Zona Sul)",
+"Criada em março de 2006, iniciou suas atividades com os cursos técnicos de Administração e de Eletrônica. No primeiro semestre de 2008 implantou o curso de Ensino Médio.",
+"1158519315",
+"https://www.cps.sp.gov.br/etecs/etec-carolina-carinhato-sampaio-jardim-sao-luis/",
+"e134dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+5,
+"Etec Cepam",
+"1133247200",
+"https://www.cps.sp.gov.br/etecs/etec-cepam-butanta/",
+"e223dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+6,
+"Etec de Artes",
+"1120890740 / 1120890741",
+"https://www.cps.sp.gov.br/etecs/etec-de-artes-santana/",
+"e180dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+7,
+"Etec de Cidade Tiradentes",
+"1125166965 / 1125166966",
+"https://www.cps.sp.gov.br/etecs/etec-cidade-tiradentes-cidade-tiradentes/",
+"e199dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+8,
+"Etec de Esportes – Curt Walter Otto Baumgart",
+"1126316584 / 1126316596",
+"https://www.cps.sp.gov.br/etecs/etec-de-esportes-curt-walter-otto-baumgart-vila-maria/",
+"e267dir@cps.sp.gov.br");
+
+
+INSERT INTO unidade (id_endereco, nome, descricao, telefone, site, email)
+VALUES (
+9,
+"Etec de Guaianazes",
+"Implantada em janeiro de 2004, a unidade está em uma região de alto índice de exclusão social e de alta densidade demográfica (acima de um milhão de habitantes), que não dispõe de locais e nem de instrumentos para pesquisa bibliográfica na área tecnológica e de tecnologia de ponta. O Governo do Estado de São Paulo, junto com o Governo Federal, através do Centro Paula Souza e do Programa de Expansão da Educação Profissional (Proep) instalou a escola técnica na região para contribuir com o aumento da empregabilidade e melhoria da qualidade de vida dos moradores de Guaianases e região; por meio do desenvolvimento das competências e pela prática da educação profissional.",
+"1125519484",
+"https://www.cps.sp.gov.br/etecs/etec-de-guaianazes-guaianazes/",
+"e118dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+10,
+"Etec de Heliópolis",
+"1120832727",
+"https://www.cps.sp.gov.br/etecs/etec-de-heliopolis-heliopolis/",
+"e205dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+11,
+"Etec de Itaquera",
+"1125218188 / 1122547627",
+"https://www.cps.sp.gov.br/etecs/etec-de-itaquera-cohab-2/",
+"e169dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+12,
+"Etec de Itaquera II",
+"1120565142 / 1120565148",
+"https://www.cps.sp.gov.br/etecs/etec-de-itaquera-ii-itaquera/",
+"e285dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+13,
+"Etec de Sapopemba",
+"1120191519 / 1120191533",
+"https://www.cps.sp.gov.br/etecs/etec-de-sapopemba-sapopemba/",
+"e172dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+14,
+"Etec de Tiquatira",
+"1122252504",
+"https://www.cps.sp.gov.br/etecs/etec-de-tiquatira-penha/",
+"e208dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+15,
+"Etec de Vila Formosa",
+"1122116485 / 1129169467",
+"https://www.cps.sp.gov.br/etecs/etec-de-vila-formosa-vila-formosa/",
+"e185dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+16,
+"Etec Dra. Maria Augusta Saraiva",
+"1131057250 / 1132240744",
+"https://www.cps.sp.gov.br/etecs/etec-dra-maria-augusta-saraiva-campos-eliseos/",
+"e154dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, descricao, telefone, site, email)
+VALUES (
+17,
+"Etec Getúlio Vargas",
+"Fundada em 1911 é voltada unicamente à formação do ensino Técnico. Desde então, teve várias denominações: Escola Profissional Masculina, Escola Profissional e Indústria, Instituto Profissional Masculino, Colégio Industrial Estadual Getúlio Vargas, Centro Interescolar de ensino Técnico e Escola Técnica Estadual Getúlio Vargas. Em 1964, desmembrou-se em três partes: Escola Estadual de Segundo Grau Martin Luther King (Tatuapé), Escola Estadual de Segundo Grau José Rocha Mendes (Vila Prudente) e Colégio Técnico (Ipiranga). Este último, em 1972, passou a se chamar Escola Técnica Estadual Getúlio Vargas, funcionando em regime de Intercomplementaridade até 1991. A partir 1982, a Etec Getúlio Vargas passou a integrar o Centro Paula Souza.",
+"1120662500",
+"https://www.cps.sp.gov.br/etecs/etec-getulio-vargas-ipiranga/",
+"e013dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+18,
+"Etec Gildo Marçal Bezerra Brandão (Etec Perus)",
+"1139178751 / 1139178263",
+"https://www.cps.sp.gov.br/etecs/etec-gildo-marcal-bezerra-brandao-perus/",
+"e226dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, descricao, telefone, site, email)
+VALUES (
+19,
+"Etec Guaracy Silveira",
+"A Etec Guaracy Silveira é uma tradicional escola técnica paulista e a única na região de Pinheiros. Foi criada em 1950 recebendo o nome de Escola Industrial de Pinheiros. Posteriormente em 1970 Ginásio Industrial Estadual Guaracy Silveira, em 1978, Escola Estadual de 2º Grau Guaracy Silveira, em 1981, Escola Técnica de 2º Grau Guaracy Silveira, sempre pertencendo a rede estadual de ensino da Secretaria da Educação. Em 1992 passou para a Secretaria da Ciência, Tecnologia e Desenvolvimento Econômico e em 1993 foi incorporada pelo Centro Paula Souza sob o nome de Etec Guaracy Silveira."
+"1130316208",
+"https://www.cps.sp.gov.br/etecs/etec-guaracy-silveira-pinheiros/",
+"e061dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+20,
+"Etec Irmã Agostina",
+"1156673971 / 1156673973",
+"https://www.cps.sp.gov.br/etecs/etec-irma-agostina-capela-do-socorro/",
+"e238dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+21,
+"Etec Jaraguá",
+"1139417242 / 1139418319",
+"https://www.cps.sp.gov.br/etecs/etec-jaragua-jaragua/",
+"e228dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+22,
+"Etec Jardim Ângela",
+"1158330943 / 1158330861",
+"https://www.cps.sp.gov.br/etecs/etec-jardim-angela-jardim-angela/",
+"e221dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+23,
+"Etec Jornalista Roberto Marinho",
+"1151032085",
+"https://www.cps.sp.gov.br/etecs/etec-jornalista-roberto-marinho-brooklin/",
+"e253dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+24,
+"Etec José Rocha Mendes",
+"1120634454",
+"https://www.cps.sp.gov.br/etecs/etec-jose-rocha-mendes-vila-prudente/",
+"e076dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+25,
+"Etec Mandaqui",
+"1129738755 / 1122836603",
+"https://www.cps.sp.gov.br/etecs/etec-mandaqui-mandaqui/",
+"e247dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, descricao, telefone, site, email)
+VALUES (
+26,
+"Etec Martin Luther King",
+"Por meio do Decreto de 1965 a escola foi criada sob o nome de Ginásio Industrial Estadual do Tatuapé, desmembramento este, do Colégio Industrial Estadual Getulio Vargas. Em 1968 a GIE - Ginásio Industrial Estadual do Bairro do Tatuapé passa a denominar-se Martin Luther King, nome este dado em homenagem ao pacifista, falecido em 1968. De 1971 a 1973, o Colégio Técnico Industrial, e em 1978 denomina-se E.E.S.G. Martin Luther King, vinculada a 7ª delegacia de Ensino. Em 1993, a escola é transferida para o Centro Estadual de Educação Tecnológica Paula Souza.",
+"1120917465",
+"https://www.cps.sp.gov.br/etecs/etec-martin-luther-king-tatuape/",
+"e085dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+27,
+"Etec Parque Belém",
+"1122920742 / 1122919293",
+"https://www.cps.sp.gov.br/etecs/etec-parque-belem-parque-belem/",
+"e220dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+28,
+"Etec Parque da Juventude",
+"1122210098 / 1122210086",
+"https://www.cps.sp.gov.br/etecs/etec-parque-da-juventude-santana/",
+"e159dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+29,
+"Etec Paulistano",
+"1139791120",
+"https://www.cps.sp.gov.br/etecs/etec-paulistano-freguesia-do-o/",
+"e229dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+30,
+"Etec Prof. Adhemar Batista Heméritas",
+"1123011058",
+"https://www.cps.sp.gov.br/etecs/etec-prof-adhemar-batista-hemeritas-vila-formosa/",
+"e207dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+31,
+"Etec Prof. Aprígio Gonzaga",
+"1126471491 / 1126478111",
+"https://www.cps.sp.gov.br/etecs/etec-prof-aprigio-gonzaga-penha/",
+"e034dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, descricao, telefone, site, email)
+VALUES (
+32,
+"Etec Prof. Basílides de Godoy",
+"Criada em 1950, sob a denominação de Escola Artesanal da Lapa. Inicialmente eram oferecidos cursos destinados ao público feminino. Em 1963, recebeu o nome de Escola Industrial da Lapa, denominação que manteve até 1966, quando seus cursos foram elevados ao nível ginasial, passando a escola a ser reconhecida como Ginásio Industrial Prof. Basilides de Godoy ou Ginásio Industrial da Lapa ou, ainda, como o Industrial da Lapa. Em 1972, mudou-se para o prédio onde funciona atualmente, sendo criado o Colégio Técnico de Vila Leopoldina. Em 1986, passou a distinguir-se das demais escolas de segundo grau do Estado pela designação Escola Técnica Estadual de Segundo Grau Prof. Basilides de Godoy. Em 1994, foi transferida para o Centro Paula Souza.",
+"1138344780 / 1138316034",
+"https://www.cps.sp.gov.br/etecs/etec-prof-basilides-de-godoy-vila-leopoldina/",
+"e041dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, descricao, telefone, site, email)
+VALUES (
+33,
+"Etec Prof. Camargo Aranha",
+"Primeiro colégio comercial da rede de ensino do Estado de São Paulo, surgiu dentro do sistema educacional brasileiro, em 1968, para suprir as necessidades do mercado de trabalho e, desde então, vem se aperfeiçoando para formar técnicos de qualidade. A escola dispõe de laboratórios de Ciências, Inglês, Informática e equipamentos didáticos para oferecer aos alunos curso práticos e interessantes, com metodologia atualizada. Promove encaminhamento a estágios e empregos.",
+"1126946733",
+"https://www.cps.sp.gov.br/etecs/etec-prof-camargo-aranha-mooca/",
+"e012dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, descricao, telefone, site, email)
+VALUES (
+34,
+"Etec Prof. Horácio Augusto da Silveira",
+"Começou as atividades em outubro de 1956, com o nome de Escola Artesanal de Vila Maria. Em 1962 passa a ser denominada Ginásio Industrial da Vila Maria e em 1965, Ginásio Industrial Estadual Prof. Horácio Augusto da Silveira. Em 1993, a escola foi incorporada ao Centro Paula Souza.",
+"1129051128 / 1129051125",
+"https://www.cps.sp.gov.br/etecs/etec-prof-horacio-augusto-da-silveira-vila-guilherme/",
+"e064dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+35,
+"Etec Profª Dra. Doroti Quiomi Kanashiro Toyohara (Etec Pirituba)",
+"1139720199 / 1139720339",
+"https://www.cps.sp.gov.br/etecs/etec-prof-a-dr-a-doroti-quiomi-kanashiro-toyohara-pirituba/",
+"e254dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+36,
+"Etec Raposo Tavares",
+"1137825782",
+"https://www.cps.sp.gov.br/etecs/etec-raposo-tavares-raposo-tavares/",
+"e225dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+37,
+"Etec Santa Ifigênia",
+"1132213984",
+"https://www.cps.sp.gov.br/etecs/etec-santa-ifigenia-santa-ifigenia/",
+"e260dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+38,
+"Etec São Mateus",
+"1127215111",
+"https://www.cps.sp.gov.br/etecs/etec-sao-mateus-sao-mateus/",
+"e227dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, descricao, telefone, site, email)
+VALUES (
+39,
+"Etec São Paulo",
+"A escola foi criada em 1988, consolidando em poucos anos sua imagem no ensino Médio e Técnico oferecendo, inclusive, cursos extracurriculares em diferentes áreas profissionais. Com a missão de formar profissionais competentes para o mundo do trabalho e para o exercício efetivo da cidadania, a Etec aposta na gestão participativa e democrática, na qual a comunidade escolar tem um papel importante na definição das metas pedagógicas e na aquisição de recursos materiais para o trabalho docente.",
+"1133260993",
+"https://www.cps.sp.gov.br/etecs/etec-sao-paulo-bom-retiro/",
+"e018dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+40,
+"Etec Sebrae",
+"1132240889 / 1132241260",
+"https://www.cps.sp.gov.br/etecs/etec-sebrae-campos-eliseos/",
+"e273dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+41,
+"Etec Takashi Morita",
+"1155247104 / 1155210636",
+"https://www.cps.sp.gov.br/etecs/etec-takashi-morita-santo-amaro/",
+"e200dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+42,
+"Etec Tereza Aparecida Cardoso Nunes de Oliveira",
+"1122171409 / 1122171547",
+"https://www.cps.sp.gov.br/etecs/etec-tereza-aparecida-cardoso-nunes-de-oliveira-arthur-alvim/",
+"e186dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+43,
+"Etec Uirapuru",
+"1137825376 / 1137824837",
+"https://www.cps.sp.gov.br/etecs/etec-uirapuru-uirapuru/",
+"e230dir@cps.sp.gov.br");
+
+INSERT INTO unidade (id_endereco, nome, telefone, site, email)
+VALUES (
+44,
+"Etec Zona Leste",
+"Criado fevereiro de 2002, o Centro Tecnológico da Zona Leste (CT-ZL) que era composto por Etec da Zona Leste, Fatec da Zona Leste e Núcleo de Pesquisa e Desenvolvimento Tecnológico. Em 2009 foi extinto o Centro Tecnológico da Zona Leste e criada a Etec da Zona Leste localizada a Av. Águia de Haia, 2633 e Fatec da Zona Leste, localizada na Av. Águia de Haia, 2933.",
+"1120454016 / 1120454017",
+"https://www.cps.sp.gov.br/etecs/etec-da-zona-leste-cidade-a-e-carvalho/",
+"e211dir@cps.sp.gov.br");
